23186e7eb1a3a252ac0061367543ad1ef8f5626a
daveluo <kitsudo163@163.com>
2021-06-07 02:24:41 +0800

    更新资源

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ def get_package():
 
 setup(
     version=f"0.1.{time.strftime('%Y%m%d%H%M', time.localtime())}",
-    packages=["base", "frameworks", "kiwi", "modules/core", "static", "migrations"],
+    packages=["base", "frameworks", "kiwi", "modules", "static", "migrations"],
     install_requires=get_package(),
 )
