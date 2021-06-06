b2e057ea67b0b8b7919465e393bb43c14c3f0603
daveluo <kitsudo163@163.com>
2021-06-06 23:59:55 +0800

    完善配置

--- setup.py
+++ setup.py
@@ -3,18 +3,6 @@ import re
 
 from setuptools import find_packages, setup
 
-READMEFILE = "README.md"
-VERSIONFILE = "_version.py"
-VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-
-
-def get_version():
-    mo = re.search(VSRE, open(VERSIONFILE, "rt").read(), re.M)
-    if mo:
-        return mo.group(1)
-    else:
-        raise RuntimeError("Unable to find version string in %s." % VERSIONFILE)
-
 
 def get_package():
     with open("requirements.txt") as fin:
@@ -22,19 +10,6 @@ def get_package():
 
 
 setup(
-    name='kiwi',
-    version=get_version(),
-    description='',
-    long_description=open(READMEFILE).read(),
-    url='',
-    author='dave luo',
-    license='BSD',
     packages=["base", "frameworks", "modules/core"],
     install_requires=get_package(),
-    package_data={
-        "static": ["static/**"]
-    },
-    include_package_data=True,
-    classifiers=[
-    ],
 )
