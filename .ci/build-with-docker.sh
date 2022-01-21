d5932877e9a0c6935609c04dba9eba7f084d5139
dave-luo <kitsudo163@163.com>
2022-01-21 08:01:40 +0800

    完善onbuild

--- .ci/build-with-docker.sh
+++ .ci/build-with-docker.sh
@@ -4,7 +4,7 @@ export IMAGE=kitsudo/kiwi-server:python-3.8
 echo "image: ${IMAGE}"
 set -x
 if [ "${NO_GFW}" = "TRUE" ]; then
-  docker build --platform linux/amd64 . -f .ci/Dockerfile -t "${IMAGE}" --build-arg PYPI_OPTIONS=""
+  docker build --platform linux/amd64 --progress plain . -f .ci/Dockerfile -t "${IMAGE}" --build-arg PYPI_OPTIONS=""
 else
-  docker build --platform linux/amd64 . -f .ci/Dockerfile -t "${IMAGE}"
+  docker build --platform linux/amd64 --progress plain . -f .ci/Dockerfile -t "${IMAGE}" --build-arg PYPI_OPTIONS="-i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com"
 fi
